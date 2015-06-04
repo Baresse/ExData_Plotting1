a3a232f6128f3768ccbb3cf1aff80f3eec45bdc9
Laurent Baresse <laurent.baresse@gmail.com>
2015-06-05 01:21:31 +0200

    Only four R sources must be committed.

--- plot1.R
+++ plot1.R
@@ -1,5 +1,37 @@
-source('./loadData.R')
+library(data.table)
 
+##
+## Function used to load the dataset
+##
+loadData <- function (savedFile = "./data/household_power_consumption.txt") {
+        
+        ## Set to C Locale
+        Sys.setlocale('LC_TIME', 'C')
+        
+        ## Read file into a data.table
+        DT <- fread(savedFile, na.strings="?", colClasses = "characters")
+        
+        ## Subset dataset
+        data <- subset(DT, DT$Date == "1/2/2007" | DT$Date  == "2/2/2007")
+        
+        ## Convert the Date column into a POSIXct class
+        data$timestamp <- paste(data$Date, data$Time)
+        data$timestamp <- as.POSIXct(strptime(data$timestamp, "%d/%m/%Y %H:%M:%S"))
+        
+        ## Convert into numeric the parameters
+        data$Global_active_power <- as.numeric(data$Global_active_power)
+        data$Global_reactive_power <- as.numeric(data$Global_reactive_power)
+        data$Sub_metering_1 <- as.numeric(data$Sub_metering_1)
+        data$Sub_metering_2 <- as.numeric(data$Sub_metering_2)
+        data$Sub_metering_3 <- as.numeric(data$Sub_metering_3)
+        data$Voltage <- as.numeric(data$Voltage)
+        
+        data
+} 
+
+##
+## Function used to generate the plot
+##
 plot1 <- function (savedFile = "./data/household_power_consumption.txt") {
 
         ## Read data from the dataset and subset it in order to keep only 2 days as required
